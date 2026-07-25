a9a063dc7dc5e0a37b04582a190879a56c6cec31
venkatasaikrishnapiduru <krishnapiduruve@outlook.com>
2026-07-25 11:31:33 -0400

    First Commit

--- hellow_world.py
+++ hellow_world.py
@@ -3,7 +3,6 @@ from datetime import datetime
 print("Hello Again ",datetime.now())
 print(str(datetime.now()))
 print("Hello Again")
-print("hellow Again")
 for func in list(dir(datetime)):
     if not func.startswith('__'):
         print(f'{func}',f'{help(func)}')
