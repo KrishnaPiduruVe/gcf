8492b03709a999f7f40ff5313bca3ac0466d2dfe
venkatasaikrishnapiduru <krishnapiduruve@outlook.com>
2026-07-25 10:53:34 -0400

    First Commit

--- hellow_world.py
+++ hellow_world.py
@@ -2,5 +2,5 @@ print("Hello World")
 from datetime import datetime
 print("Hello Again ",datetime.now())
 print(str(datetime.now()))
-#for func in list(dir(datetime)):
-    #print(func)
+for func in list(dir(datetime)):
+    print(func)
