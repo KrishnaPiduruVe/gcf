6d4f79a706570065e1a049c642e0c11bf18aa87d
venkatasaikrishnapiduru <krishnapiduruve@outlook.com>
2026-07-25 10:47:33 -0400

    First Commit

--- hellow_world.py
+++ hellow_world.py
@@ -1,3 +1,16 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+print("*"*30)
+url = "https://hooks.slack.com/services/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"
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json"
+data ='{"from":"pycharm"}'
+req_post_response = requests.post(url,headers=headers,data=data)
+print(req_post_response.status_code)
+print("*"*30)
+
+
+
 print("Hello World")
 from datetime import datetime
 print("Hello Again ",datetime.now())
